4d8218d4f6f42364957d4fb52b3b9cf375e25e5c
Senthil Kumar <senthilkumar@Senthils-MacBook-Pro.local>
2016-10-29 16:52:31 +0530

    logges port

--- tools/utils/seed/server.ts
+++ tools/utils/seed/server.ts
@@ -59,7 +59,7 @@ export function serveCoverage() {
  * Starts a new `express` server, serving the built files from `dist/prod`.
  */
 export function serveProd() {
-  let root = resolve(process.cwd(), Config.PROD_DEST);
+  let root = resolve(Config.PROD_DEST);
   let server = express();
   console.log('------------------------------');
   console.log('heroku dir : '+process.cwd());
