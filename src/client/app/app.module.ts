a92e30ccdb5654597681741f6e5d6ffb5a1b1c41
Senthil Kumar <senthilkumar@Senthils-MacBook-Pro.local>
2016-10-30 23:01:31 +0530

    created home component with initial styling and structure

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -4,10 +4,11 @@ import { APP_BASE_HREF } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [ BrowserModule, NgbModule.forRoot() ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, HomeComponent ],
   providers: [{
     provide: APP_BASE_HREF,
     useValue: '<%= APP_BASE %>'
