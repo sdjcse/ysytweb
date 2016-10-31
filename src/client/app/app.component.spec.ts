fb537c66dcc953dba717be083047559770b8b4f7
Senthil Kumar <senthilkumar@Senthils-MacBook-Pro.local>
2016-10-31 06:04:09 +0530

    style sheet link issue in component file fixed

--- src/client/app/app.component.spec.ts
+++ src/client/app/app.component.spec.ts
@@ -3,13 +3,14 @@ import { async, TestBed } from '@angular/core/testing';
 import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 export function main() {
 
   describe('App component', () => {
     beforeEach(() => {
       TestBed.configureTestingModule({
-        declarations: [ TestComponent, AppComponent ],
+        declarations: [ TestComponent, AppComponent, HomeComponent ],
         providers: [
           { provide: APP_BASE_HREF, useValue: '/' }
         ]
