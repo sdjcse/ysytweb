d68256def4e9cb9af9ab2cbdef31c5bcf8ba95c8
Senthil Kumar <senthilkumar@Senthils-MacBook-Pro.local>
2016-11-01 23:04:03 +0530

    bootstrap lib added

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -28,7 +28,7 @@ export class ProjectConfig extends SeedConfig {
     this.APP_ASSETS = [
       ...this.APP_ASSETS,
       // {src: `${this.APP_SRC}/your-path-to-lib/libs/jquery-ui.js`, inject: true, vendor: false}
-      // {src: `${this.CSS_SRC}/path-to-lib/test-lib.css`, inject: true, vendor: false},
+      {src: `${this.CSS_SRC}/vendor/bootstrap/bootstrap.css`, inject: true, vendor: false}
     ];
 
     /* Add to or override NPM module configurations: */
