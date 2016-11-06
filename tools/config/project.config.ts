ff2c75e62a023d270ed339d74230dec635b5d794
Senthil Kumar <senthilkumar@Senthils-MacBook-Pro.local>
2016-11-06 16:38:24 +0530

    added font awesome to the package.json and created task for building fonts

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -9,6 +9,8 @@ import { SeedConfig } from './seed.config';
 export class ProjectConfig extends SeedConfig {
 
   PROJECT_TASKS_DIR = join(process.cwd(), this.TOOLS_DIR, 'tasks', 'project');
+  FONTS_DEST = `${this.APP_DEST}/fonts`;
+  FONTS_SRC = [ 'node_modules/font-awesome/fonts/**' ];
 
   constructor() {
     super();
